b7f6e5d47b914525ec5118738c1d40476238ab20
suhas <suhassahadevan02@gmail.com>
2024-06-14 10:45:34 +0530

    Changes

--- setup.py
+++ setup.py
@@ -11,24 +11,24 @@ class PostInstallCommand(install):
         subprocess.check_call([sys.executable, 'post_install.py'])
 
 setup(
-    name="FileUploadAPI",
+    name="UploadAPI",
     version="1.0",
     packages=find_packages(),
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'run_fileUploadAPI=fileUploadAPI.app:main',
+            'run_UploadAPI=UploadAPI.app:main',
         ],
     },
     package_data={
         '': ['templates/*.html'],
     },
-    author="Sushma",
-    author_email="saisushma.patange@gmail.com",
+    author="Suhas",
+    author_email="suhassahadevan02@gmail.com",
     description="This API is used to accept file uploads from end users.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url="https://github.com/P-Sai-Sushma/FileUploadAPI.git",
+    url="https://github.com/SuhasSahadev/Uploadapi.git",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
